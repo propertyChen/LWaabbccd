c765b9309262ffe3a44feefc1b9b02a5e2a48376
chenjunda <chenjunda@talkmoney.cn>
2019-11-08 17:01:01 +0800

    first commit

---
+++ Example/Pods/Target Support Files/LWaabbccd/LWaabbccd-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LWaabbccd : NSObject
+@end
+@implementation PodsDummy_LWaabbccd
+@end
---
+++ Example/Pods/Target Support Files/Pods-LWaabbccd_Example/Pods-LWaabbccd_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LWaabbccd_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LWaabbccd_Example
+@end
